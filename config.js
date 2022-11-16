c90ef8171737e77852d145b7ba5fa5b5f17d009f
Doanthuat <95136439+Doanthuat@users.noreply.github.com>
2022-11-16 21:11:54 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hôm nay là sinh nhật của em phải hông chứ!',
+    introDesc: ` Ngày sinh nhật em, anh chúc em luôn khỏe mạnh, tươi trẻ và công tác tốt, Oanh...sinh nhật vui vẻ nha em.`,
+    btnIntro: 'Em bấm vào đi!',
+    title: 'Nay là ngày 16/11/2022 cũng là ngày sinh nhật của em 🥳',
+    desc: 'Phải chăng anh đuợc gặp em:333 ',
+    btnYes: 'Vẫn cứ là yêu anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'vì em yêu anh Thuật quá trời  <33333333',
+    mess: 'Anh biết Oanh nói vậy mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'anh sẽ bù em vào ngày nào nhé baby 😘',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/1441398283' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
